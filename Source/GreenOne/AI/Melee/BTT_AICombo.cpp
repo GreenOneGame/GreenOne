635fe76f73ca77d7b22e3da36c85e09ef14a20d6
idryssj31 <idryss3108@gmail.com>
2023-05-04 01:24:12 +0200

    Some Change But Still Not All Fixe
    quand on dodge la premiere attaque l'ia fonce tout droit

--- Source/GreenOne/AI/Melee/BTT_AICombo.cpp
+++ Source/GreenOne/AI/Melee/BTT_AICombo.cpp
@@ -15,13 +15,6 @@ UBTT_AICombo::UBTT_AICombo()
 
 EBTNodeResult::Type UBTT_AICombo::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	if(IsValid(&OwnerComp))
-	{
-		// Log
-		UE_LOG(LogTemp, Warning, TEXT("BTT_AICombo::ExecuteTask"));
-		return EBTNodeResult::Failed;
-	}
-	
 	FightMStatus = (UKismetMathLibrary::RandomBool() ? (-1.f) : (1.f));
 	SetMoveFight(OwnerComp);
 	FTimerHandle Timer;
@@ -29,86 +22,90 @@ EBTNodeResult::Type UBTT_AICombo::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
 	{
 		//OnTaskFinished(OwnerComp, NodeMemory, EBTNodeResult::Succeeded);
 		//FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
-		AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn());
-		UE_LOG(LogTemp, Warning, TEXT("IA_Ref : %s"), *IA_Ref->GetName());
-		
-		if(IsValid(IA_Ref))
+		if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 		{
-			IA_Ref->CanM_Fighting = false;
-			IA_Ref->CanMR_Fighting = false;
+			PlayerRef->CanM_Fighting = false;
+			PlayerRef->CanMR_Fighting = false;
 			HitCheck(OwnerComp);
 		}
-
+		UE_LOG(LogTemp, Warning, TEXT("timer"));
 	},1.5f, false);
 	return EBTNodeResult::InProgress;
 }
 
 void UBTT_AICombo::OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTNodeResult::Type TaskResult)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
+		UE_LOG(LogTemp, Warning, TEXT("taskfinish"));
 		FightStatus = 0;
-		IA_Ref->CanCombo = false;
-		IA_Ref->CanM_Fighting = false;
-		IA_Ref->CanMR_Fighting = false;
-		IA_Ref->Can_Fighting = false;
-		IA_Ref->CanR_Fighting = false;
-		IA_Ref->StopMov();
+		PlayerRef->CanCombo = false;
+		PlayerRef->CanM_Fighting = false;
+		PlayerRef->CanMR_Fighting = false;
+		PlayerRef->Can_Fighting = false;
+		PlayerRef->CanR_Fighting = false;
+		PlayerRef->StopMouv();
 	}
 }
 
+
 void UBTT_AICombo::SetMoveFight(UBehaviorTreeComponent& OwnerComp)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
-		IA_Ref->CanM_Fighting = true;
+		PlayerRef->CanM_Fighting = true;
 		if(FightMStatus == -1)
 		{
+			UE_LOG(LogTemp, Warning, TEXT("mouv gauche"));
 			FightStatus = -1;
 		}
 		else if(FightMStatus == 1)
 		{
-			IA_Ref->CanMR_Fighting = true;
+			UE_LOG(LogTemp, Warning, TEXT("mouv droite"));
+			PlayerRef->CanMR_Fighting = true;
 			FightStatus = 1;
 		}
 		Check(OwnerComp);
 	}
 }
 
+
 void UBTT_AICombo::SetFight(UBehaviorTreeComponent& OwnerComp)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
-		IA_Ref->Can_Fighting = true;
+		UE_LOG(LogTemp, Warning, TEXT("setfight"));
+		PlayerRef->Can_Fighting = true;
 		if (FightMStatus == -1)
-		{
-			IA_Ref->CanR_Fighting = true;
-		}
+			{
+				PlayerRef->CanR_Fighting = true;
+				UE_LOG(LogTemp, Warning, TEXT("id_fightR"));
+			}
 		if (FightMStatus == 1)
-		{
-			// LOG DE MERDE
-			UE_LOG(LogTemp, Warning, TEXT("id_fightL"));
-		}
+			{
+				
+				UE_LOG(LogTemp, Warning, TEXT("id_fightL"));
+			}
 		FTimerHandle Timer;
 		GetWorld()->GetTimerManager().SetTimer(Timer, [&]()
 		{
 			FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
 		},2.0f, false);
 	}
+	
 }
+
 
 void UBTT_AICombo::Check(UBehaviorTreeComponent& OwnerComp)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
-		if(	IA_Ref->CanM_Fighting == false)
+		if(	PlayerRef->CanM_Fighting == false)
 		{
-			// Log de merde
 			UE_LOG(LogTemp, Warning, TEXT("mouvL faux"));
 		}
-		if(	IA_Ref->CanMR_Fighting == false)
+		if(	PlayerRef->CanMR_Fighting == false)
 		{
-			// Log de merde
 			UE_LOG(LogTemp, Warning, TEXT("mouvR faux"));
 		}
 	}
@@ -117,23 +114,27 @@ void UBTT_AICombo::Check(UBehaviorTreeComponent& OwnerComp)
 
 void UBTT_AICombo::HitCheck(UBehaviorTreeComponent& OwnerComp)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
-		if(	IA_Ref->CanCombo == true)
+		if(	PlayerRef->CanCombo == true)
 		{
-			IA_Ref->StopMov();
+			PlayerRef->StopMouv();
+			UE_LOG(LogTemp, Warning, TEXT("Can Combo"));
 			SetFight(OwnerComp);
 		}
+		else{UE_LOG(LogTemp, Warning, TEXT("cest la merde"))};
 	}
 }
 
 void UBTT_AICombo::Reset(UBehaviorTreeComponent& OwnerComp)
 {
-	if(AMeleeAICharacter* IA_Ref = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
+
+	if(AMeleeAICharacter* PlayerRef = Cast<AMeleeAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
-		IA_Ref->CanM_Fighting = false;
-		IA_Ref->CanMR_Fighting = false;
-		IA_Ref->StopMov();
+		UE_LOG(LogTemp, Warning, TEXT("Reset Value Mouv"));
+		PlayerRef->CanM_Fighting = false;
+		PlayerRef->CanMR_Fighting = false;
+		PlayerRef->StopMouv();
 		HitCheck(OwnerComp);
 	}
 }
